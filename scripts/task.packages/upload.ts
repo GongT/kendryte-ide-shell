3fc8d8307325543d8266a406b2783326667bd0bc
GongT <gongteng524702837@gmail.com>
2019-01-01 13:17:04 +0800

    split

--- scripts/task.packages/upload.ts
+++ scripts/task.packages/upload.ts
@@ -11,7 +11,10 @@ const uploadPackage = everyPlatform('offpack:upload', [createZipFiles], (platfor
 	return gulpSrc(BUILD_ARTIFACTS_DIR, offlinePackageFileName(platform))
 		.pipe(gulpS3.dest({base: AWS_RELEASE_PACKAGES_PATH}));
 });
-everyPlatform('offpack', [uploadPackage], updateRelease);
+everyPlatform('offpack', [uploadPackage], () => {
+
+});
+task('offpack:update.json:only', [], updateRelease);
 
 export const modifyJsonTask = task('offpack:update.json', [uploadPackage], updateRelease);
 
