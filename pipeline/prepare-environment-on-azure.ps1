5dfe0ffd8216e483f85663cd7a678896a9953563
GongT <admin@gongt.me>
2019-01-08 18:42:46 +0800

    host -> output

--- pipeline/prepare-environment-on-azure.ps1
+++ pipeline/prepare-environment-on-azure.ps1
@@ -24,36 +24,36 @@ function downloadFile()
 	
 	if ( !(Test-Path -Path $resultDownload) )
 	{
-		Write-Host "Downloading file From: $Uri, To: $resultDownload"
+		Write-Output "Downloading file From: $Uri, To: $resultDownload"
 		$tempDownload = "${resultDownload}.partial"
 		[ Net.ServicePointManager ]::SecurityProtocol = [ Net.SecurityProtocolType ]::Tls12
 		if ( $env:HTTPS_PROXY )
 		{
-			Write-Host "Using proxy: $env:HTTPS_PROXY"
+			Write-Output "Using proxy: $env:HTTPS_PROXY"
 			Invoke-WebRequest -Uri $Uri -OutFile $tempDownload -Proxy $env:HTTPS_PROXY
 		}
 		else
 		{
-			Write-Host "Using direct connection."
+			Write-Output "Using direct connection."
 			Invoke-WebRequest -Uri $Uri -OutFile $tempDownload
 		}
-		Write-Host "Downloaded file: $resultDownload"
+		Write-Output "Downloaded file: $resultDownload"
 		Rename-Item -Path $tempDownload -NewName $resultDownload -Force
 	}
 	else
 	{
-		Write-Host "Exists file: $resultDownload"
+		Write-Output "Exists file: $resultDownload"
 	}
 }
 
 if ( $env:AGENT_OS -eq "Linux" )
 {
-	Write-Host "LinuxPath is $env:PATH"
+	Write-Output "LinuxPath is $env:PATH"
 	sudo apt-get install p7zip-full
 }
 ElseIf ($env:AGENT_OS -eq "Darwin")
 {
-	Write-Host "MacPath is $env:PATH"
+	Write-Output "MacPath is $env:PATH"
 	$TMP = $env:TMPDIR
 	downloadFile 'https://registry.npmjs.org/7zip-bin/-/7zip-bin-4.1.0.tgz' "$TMP/7zb.tar.gz"
 	exec { tar xf "$TMP/7zb.tar.gz" -C $TMP } "Cannot extract 7zip-bin tar.gz"
@@ -66,23 +66,23 @@ Else
 {
 	# windows
 	$TMP = $env:TMP
-	Write-Host "WindowsPath is $env:PATH"
+	Write-Output "WindowsPath is $env:PATH"
 	
 	exec { npm install '@kendryte-ide/windows-python2' '7zip' }
 	
 	$python = exec { yarn --silent run python -c 'import sys; print sys.executable' }
-	Write-Host "Python is at $python"
+	Write-Output "Python is at $python"
 	$pythonCallScript = @"
 `"$python`" %*
 "@
-	Write-Host $pythonCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/python.bat" -Encoding "ascii"
+	Write-Output $pythonCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/python.bat" -Encoding "ascii"
 	
-	Write-Host "7Zip is at $PSScriptRoot\node_modules\7zip\7zip-lite\7z.exe"
+	Write-Output "7Zip is at $PSScriptRoot\node_modules\7zip\7zip-lite\7z.exe"
 	$7zipCallScript = @"
 `"$PSScriptRoot\node_modules\7zip\7zip-lite\7z.exe`" %*
 "@
-	Write-Host $7zipCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/7z.bat" -Encoding "ascii"
-	Write-Host $7zipCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/7za.bat" -Encoding "ascii"
+	Write-Output $7zipCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/7z.bat" -Encoding "ascii"
+	Write-Output $7zipCallScript.Replace( "`n", "`r`n" ) | Out-File -FilePath "C:/Windows/7za.bat" -Encoding "ascii"
 	
 	Remove-Item -Recurse -Force $PSScriptRoot\node_modules\.bin
 	
@@ -100,4 +100,4 @@ $response.Headers.AllKeys |
 	Select-Object @{ Name = "Key"; Expression = { $_ } },
 	@{ Name = "Value"; Expression = { $headers.GetValues( $_ ) } }
 $sr = new-object System.IO.StreamReader $response.GetResponseStream()
-Write-Host $sr.ReadToEnd()
+Write-Output $sr.ReadToEnd()
