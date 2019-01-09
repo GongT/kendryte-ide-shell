628544314f1387dd44214bdd579b218d7a38574c
GongT <admin@gongt.me>
2019-01-09 19:35:31 +0800

    fix path

--- pipeline/create-artifacts.js
+++ pipeline/create-artifacts.js
@@ -5,15 +5,18 @@ const {execSync} = require('child_process');
 const platformName = platform();
 const builtName = platformName === 'darwin'? platformName : platformName + '-x64';
 
-const distFile = `latest-${process.env.CHANNEL}-${process.env.NODE_PLATFORM}.7z`;
+const distFile = `latest-${process.env.CHANNEL}-${platformName}.7z`;
 const distFolder = process.env.BUILD_ARTIFACTSTAGINGDIRECTORY || process.env.TMP;
 
 process.env.LANG = 'C';
 process.env.LC_ALL = 'C';
 
-const makeResult = resolve(process.cwd(), `../VSCode-${process.env.NODE_BUILTNAME}`);
+const makeResult = resolve(process.cwd(), `../VSCode-${builtName}`);
 
-execSync(`7za a -y -r -mmt -ms=on -mx3 -ssc "${distFolder}/${distFile}" .`, {
+const cmd = `7za a -y -r -mmt -ms=on -mx3 -ssc "${distFolder}/${distFile}" .`;
+console.log('Command To Run:', cmd);
+console.log('Working Dir:', makeResult);
+execSync(cmd, {
 	cwd: makeResult,
 	stdio: 'inherit',
 });
