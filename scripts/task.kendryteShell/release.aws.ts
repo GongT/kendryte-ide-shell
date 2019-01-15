07c9bd51b75be47d9754c8b2e8319c7a7ba25a3d
GongT <admin@gongt.me>
2019-01-15 17:57:12 +0800

    add release version note

--- scripts/task.kendryteShell/release.aws.ts
+++ scripts/task.kendryteShell/release.aws.ts
@@ -1,5 +1,5 @@
 import { BUILD_ARTIFACTS_DIR } from '../environment';
-import { debug, gulpSrc, jeditor, mergeStream, platforms, task } from '../library/gulp';
+import { debug, gulpSrc, jeditor, log, mergeStream, platforms, task } from '../library/gulp';
 import { gulpS3 } from '../library/gulp/aws';
 import { updaterFileName } from '../library/paths/updater';
 import { getReleaseChannel } from '../library/releaseInfo/qualityChannel';
@@ -23,6 +23,7 @@ const awsUploadZipFilesTask = task('aws:upload', [compressTasks], realAwsUpload)
 
 export const awsModifyJsonTask = task('aws:update.json', [awsUploadZipFilesTask], () => {
 	const version = createReleaseTag();
+	log('using version string: %s', version);
 	return gulpS3.src(`release/IDE.${getReleaseChannel()}.json`)
 	             .pipe(jeditor({
 		             updaterVersion: version,
