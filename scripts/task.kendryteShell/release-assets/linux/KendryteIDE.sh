64db8d4f3942dce44d1ec85455828da8622f287e
GongT <admin@gongt.me>
2019-05-10 14:20:21 +0800

    linux: prevent use root

--- scripts/task.kendryteShell/release-assets/linux/KendryteIDE.sh
+++ scripts/task.kendryteShell/release-assets/linux/KendryteIDE.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ "$(id -u)" -eq 0 ]]; then
+	echo -e "\e[38;5;9mYou are not allowed to run this app with root permissions.\e[0m" >&2
+	exit 1
+fi
+
 exec "$(dirname "${BASH_SOURCE[0]}")/Updater/electron" "$@"
