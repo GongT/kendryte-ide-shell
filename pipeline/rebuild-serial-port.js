5c1088d27e276d801b8e66c2a3f4e11d40f8787f
GongT <admin@gongt.me>
2019-05-30 12:04:31 +0800

    fix rimraf

--- pipeline/rebuild-serial-port.js
+++ pipeline/rebuild-serial-port.js
@@ -1,15 +1,24 @@
 const {execSync} = require('child_process');
 const rimraf = require('rimraf');
+
+const isCI = !!process.env.SYSTEM_COLLECTIONID;
+if (!isCI) {
+	console.error('this script is for azure pipelines');
+	process.exit(1);
+}
 process.chdir(process.env.SYSTEM_DEFAULTWORKINGDIRECTORY);
 
 process.chdir('node_modules/@serialport/bindings');
-rimraf('build');
 
 console.log('run gyp rebuild in %s', process.cwd());
-console.log('npm_config_target=%s', process.env.npm_config_target);
+
+const electronVersion = require(process.env.SYSTEM_DEFAULTWORKINGDIRECTORY + '/build/lib/electron').getElectronVersion();
+console.log('npm_config_target=%s', electronVersion);
+
+rimraf.sync('build');
 execSync('node-gyp', [
 	'rebuild',
-	'--target=' + process.env.npm_config_target,
+	'--target=' + electronVersion,
 	'--arch=x64',
 	'--dist-url=https://atom.io/download/electron',
 ], {
